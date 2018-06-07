e55b2d57dbe0ca2e5e386a2d963c37caa42a7844
ghildreth <gabrielantonhildreth@gmail.com>
2018-06-06 17:32:18 -0700

    add static build for efor express server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
+
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/hello', (req, res)=> {
     res.send({ express: 'Hello From Express' });
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 app.listen(port, () => console.log(`Listening on ${port}`))
